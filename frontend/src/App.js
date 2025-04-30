dfcb7fbac69fd555605b76e6bb096a2428677934
Shaina Nkouankam <shainankm@gmail.com>
2025-04-30 16:28:36 +0200

    Frontend corrigé prêt pour Vercel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,7 @@ import React from 'react';
 import Dashboard from './components/Dashboard';
 
 function App() {
-  return (
-    <div className="App">
-      <Dashboard />
-    </div>
-  );
+  return <Dashboard />;
 }
 
 export default App;
