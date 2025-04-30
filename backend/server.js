9d7d1d60d74e5fde6ea49e9afe9e0007cfe30ea2
Shaina Nkouankam <shainankm@gmail.com>
2025-04-30 14:56:11 +0200

    Utilise le port dynamique de Render

--- backend/server.js
+++ backend/server.js
@@ -184,7 +184,7 @@ app.post('/delete-contact', async (req, res) => {
   }
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`🚀 Serveur backend lancé sur http://localhost:${PORT}`);
 });
