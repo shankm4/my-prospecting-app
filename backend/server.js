a6c7407a202b42b7a2b4605582aa1dfae0b6fa94
Shaina Nkouankam <shainankm@gmail.com>
2025-06-04 13:52:03 +0200

    Fix CORS policy

--- backend/server.js
+++ backend/server.js
@@ -28,16 +28,6 @@ app.use(cors({
 }));
 
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://my-prospecting-app.vercel.app");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Credentials", "true");
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  next();
-});
 
 const upload = multer({ dest: 'uploads/' });
 
